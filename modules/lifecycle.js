1ef8029829e55080859d49529cefaa403cddc1ae
Stephen <stephband@cruncher.ch>
2022-06-03 17:07:50 +0200

    Get current state from URL

--- modules/lifecycle.js
+++ modules/lifecycle.js
@@ -1,5 +1,6 @@
 
-import events   from '../../dom/modules/events.js';
+import get    from '../../fn/modules/get.js';
+import events from '../../dom/modules/events.js';
 
 function isHashRef(ref) {
     return /^#\S+$/.test(ref);
@@ -7,6 +8,18 @@ function isHashRef(ref) {
 
 export default {
     construct: function() {
+        const children = this.children;
+        let n = -1;
+        while (hrefs[++n]) {
+            const option = children[n];
+            const url    = new URL(option.value, window.location);
+            if (url.href === window.location.href) {
+                // Select option as <select> value (is this the best way to do it?)
+                this.value = option.value;
+                break;
+            }
+        }
+
         events('change', this)
         .map((e) => e.target.value)
         // Ignore links for empty spaces
